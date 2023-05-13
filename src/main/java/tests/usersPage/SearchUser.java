ef4a4a020795502c4b5fc3923bbad5912b4f047e
Dell <Dell@DESKTOP-JR3SS3H>
2023-05-13 12:07:02 +0530

    javaPomGitUpload

---
+++ src/main/java/tests/usersPage/SearchUser.java
@@ -0,0 +1,27 @@
+package tests.usersPage;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.UsersPage;
+import tests.BaseTest;
+
+public class SearchUser extends BaseTest {
+	UsersPage usersPage;
+	
+	@Test(dataProvider = "Username-Provider")
+	public void searchUser(String username, boolean result) {
+		usersPage = new UsersPage(driver);
+		Assert.assertEquals(usersPage.searchUser(username), result);
+	}
+	
+	@DataProvider (name = "Username-Provider")
+	public Object[][] getData() {
+        return new Object[][]{
+                {"Admin", true},
+                {"kapil", false},
+                {"Alice.Duval", true}
+        };
+    }
+
+}
